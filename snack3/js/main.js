7897c34310db2c4197f66dba937d94b16ac7c390
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-03-10 17:15:20 +0100

    filter snack 3

--- snack3/js/main.js
+++ snack3/js/main.js
@@ -1,2 +1,31 @@
+let arrayGenerato = [];
 
+
+funzione()
+
+console.log(arrayGenerato);
+
+function funzione() {
+    arrayGenerato[0] = prompt("inserisci la prima città");
+    arrayGenerato[1] = prompt("inserisci la seconda città");
+    arrayGenerato[2] = prompt("inserisci la terza città");
+    const primoNumero = parseInt(prompt("inserisci il primo numero"));
+    const secondoNumero = parseInt(prompt("inserisci il secondo numero"));
+
+    let arraySelezionato = "";
+    arraySelezionato = arrayGenerato.filter((citta) => {
+        if (citta) {
+            console.log("arrayselezionato " + arraySelezionato);
+            return true;
+            
+        }
+        console.log("arrayselezionato " + arraySelezionato);
+        return false;
+        
+    });
+
+
+
+    return arrayGenerato;
+}
 
